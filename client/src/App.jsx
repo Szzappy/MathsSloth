f9560f6254dedad5002cc986789e95a76043f59d
Luke Szappanyos <33877119+Szzappy@users.noreply.github.com>
2025-09-30 15:29:40 +0100

    Update BrowserRouter
    User is now directed to login page upon entering and not an empty '/' page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
  return (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
