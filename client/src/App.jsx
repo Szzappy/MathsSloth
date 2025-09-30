9f06bce6d55eabd0274998088b3c2140ffdbe7bb
Luke Szappanyos <33877119+Szzappy@users.noreply.github.com>
2025-09-30 15:41:06 +0100

    Import Navigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx"
